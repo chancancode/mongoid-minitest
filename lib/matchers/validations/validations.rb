02888125729a8aa8e7554e1bad8faa6f9ba4cf74
Godfrey Chan <godfreykfc@gmail.com>
2012-08-14 16:34:21 -0700

    Added on option for validation matchers

--- lib/matchers/validations/validations.rb
+++ lib/matchers/validations/validations.rb
@@ -12,6 +12,11 @@ module Mongoid
           self
         end
 
+        def on(*contexts)
+          @expected_on = contexts.flatten.compact
+          self
+        end
+
         def matches?(subject)
           @klass     = class_of(subject)
           @validator = detect_validator
@@ -19,6 +24,7 @@ module Mongoid
 
           check_validator
           check_message if @expected_message
+          check_on if @expected_on
 
           @result
         end
@@ -34,6 +40,7 @@ module Mongoid
         def description
           desc = "validate #{@type.inspect} of #{@field.inspect}"
           desc << " with message: #{@expected_message.inspect}" if @expected_message
+          desc << " on #{@expected_on.empty? ? 'all actions' : @expected_on.map(&:inspect).join(', ')}" if @expected_on
 
           desc
         end
@@ -42,10 +49,10 @@ module Mongoid
 
         def check_validator
           if @validator
-            @negative_message = "#{@type.inspect} validator on #{@field.inspect}"
-            @positive_message = "#{@type.inspect} validator on #{@field.inspect}"
+            @negative_message = "#{@type.inspect} validator for #{@field.inspect}"
+            @positive_message = "#{@type.inspect} validator for #{@field.inspect}"
           else
-            @negative_message = "no #{@type.inspect} validator on #{@field.inspect}"
+            @negative_message = "no #{@type.inspect} validator for #{@field.inspect}"
             @result = false
           end
         end
@@ -56,6 +63,16 @@ module Mongoid
             @positive_message << " with message: #{error_message.inspect}"
           else
             @negative_message << " with message: #{error_message.inspect}"
+            @result = false
+          end
+        end
+
+        def check_on
+          on = [@validator.options[:on]].flatten.compact
+          if on.sort == @expected_on.sort
+            @positive_message << " on #{on.empty? ? 'all actions' : on.map(&:inspect).join(', ')}"
+          else
+            @negative_message << " on #{on.empty? ? 'all actions' : on.map(&:inspect).join(', ')}"
             @result = false
           end
         end
